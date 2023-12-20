88e2cfc5643fa930fc6373f0f3940dc8e1cd8738
Mac <mac@Macs-MacBook-Air.local>
2023-12-20 18:19:54 +0700

    add domashka

--- Program.cs
+++ Program.cs
@@ -1,115 +1,53 @@
-﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-// и возвращает значение этого элемента или же указание, что такого элемента нет.
-// 4 3 1 (1,2) => 9
-// 2 6 9 
-// using System.Linq;
-// using System.Text.RegularExpressions;
+﻿// Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива
+// a b c => “abcdef”
+// d e f 
 
-// int[,] array = new int[2, 3];
+// char[,] matrix = new char[2, 3] { { 'a', 'b', 'c' }, { 'd', 'e', 'f' } };
+// string str = " ";
 
-// void CreateArray()
+// for (int i = 0; i < matrix.GetLength(0); i++)
 // {
-//     for (int i = 0; i < array.GetLength(0); i++)
+//     for (int j = 0; j < matrix.GetLength(1); j++)
 //     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = new Random().Next(1, 10);
-//         }
+//             str += matrix[i,j];
 //     }
 // }
-// CreateArray();
+// System.Console.WriteLine(str);
 
-// void PrintArray()
+// Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. // “aBcD1ef!-” => “abcd1ef!-”
+
+// string str1 = "aBcD1ef!-";
+// string str2 = str1.ToLower(); 
+// System.Console.WriteLine(str2);
+
+// // Задайте произвольную строку. Выясните,
+// // является ли она палиндромом.
+
+// System.Console.WriteLine("введите строку");
+// string str = Console.ReadLine();
+
+// int n = str.Length / 2;
+// bool palindrome = false;
+// for (int i = 0; i < n; i++)
 // {
-//     for (int i = 0; i < array.GetLength(0); i++)
+//     if (str[i] != str[str.Length - 1 - i])
 //     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             System.Console.Write(array[i, j] + " ");
-//         }
-//         System.Console.WriteLine();
+//         palindrome = false;
+//         break;
 //     }
-//     System.Console.WriteLine();
+//     else
+//     {
+//         palindrome = true;
+//     }
 // }
-
-// PrintArray();
-
-// Console.WriteLine("введите строку элемента");
-// int a = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("введите столбец элемента");
-// int b = Convert.ToInt32(Console.ReadLine());
-
-// if(a > 2 || b > 3)
-// Console.WriteLine("такого элемента нет");
+// if (palindrome == true)
+// {
+//     System.Console.WriteLine("yes");
+// }
 // else
 // {
-//     System.Console.WriteLine(array[a,b]);
+//     System.Console.WriteLine("no");
 // }
-
-
-// Задайте двумерный массив. Напишите
-// программу, которая поменяет местами первую и
-// последнюю строку массива. 
-// 4 3 1 => 4 6 2
-// 2 6 9    2 6 9
-// 4 6 2    4 3 1
-
-// int[,] array = new int[2, 3];
-// int[,] array2 = new int[2, 3];
-
-// void CreateArray()
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = new Random().Next(1, 10);
-//         }
-//     }
-// }
-// CreateArray();
-
-// void PrintArray(int [,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             System.Console.Write(array[i, j] + " ");
-//         }
-//         System.Console.WriteLine();
-//     }
-//     System.Console.WriteLine();
-// }
-// PrintArray(array);
-
-// int[,] FlipArray()
-// {
-//     int temp = 0;
-//     {
-//         for (int i = 0; i < array.GetLength(1); i++)
-//         {
-//             temp = array[0, i];
-//             array[0, i] = array[1,i];
-//             array[1, i] = temp;
-//         }
-//     }
-//     return array;
-// }
-// FlipArray();
-
-// void PrintArray2()
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             System.Console.Write(array[i,j] +" ");
-//         }
-//         System.Console.WriteLine();
-//     }
-//     System.Console.WriteLine();
-// }
-// PrintArray2();
-
